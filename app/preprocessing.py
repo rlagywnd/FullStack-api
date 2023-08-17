6a35912a639ac5d74b0f3428d8187caf62be6667
jung050302 <jung200532@gmail.com>
2023-08-17 12:12:45 +0900

    판다스 데이터 전처리

--- app/preprocessing.py
+++ app/preprocessing.py
@@ -2,11 +2,32 @@ import pandas as pd
 import requests 
 
 
+def add_url(row):
+    return f'https://www.imdb.com/title/tt{row}/'
+
+def add_rating(df):
+    ratings_df=pd.read_csv('data/ratings.csv')
+    ratings_df['movieId'] = ratings_df['movieId'].astype(str)
+    agg_df=ratings_df.groupby('movieId').agg(
+        ratings_count=('rating','count'),
+        ratings_avf=('rating','mean')
+    ).reset_index();
+    
+    ratings_added_df=df.merge(agg_df,on='movieId')
+    return ratings_added_df;
+
 if(__name__=="__main__"):
     movies_df=pd.read_csv('data/movies.csv');
-    
+    # id를 문자로 인식하게 만든다
     movies_df['movieId'] = movies_df['movieId'].astype(str)
     links_df=pd.read_csv('data/links.csv',dtype=str)
-    merges_df=movies_df.merge(links_df,on='movieId',how='left');
+    merged_df=movies_df.merge(links_df,on='movieId',how='left');
+    merged_df['url']=merged_df['imdbId'].apply(lambda x:add_url(x))
+    result_df=add_rating(merged_df)
     
-    print(merges_df);
+    print(merged_df);
+    print(merged_df.columns)
+    print("-------------------------------\n\n")
+    print(merged_df.iloc[1,:])
+    print(result_df);
+    
