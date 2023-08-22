b8e1d54928cd3375a2ded12212367295feefa80c
jung050302 <jung200532@gmail.com>
2023-08-22 14:17:19 +0900

    csv파일

--- app/preprocessing.py
+++ app/preprocessing.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import requests 
+import sys
+from tqdm import tqdm
+import time
 
 
 def add_url(row):
@@ -16,6 +19,18 @@ def add_rating(df):
     ratings_added_df=df.merge(agg_df,on='movieId')
     return ratings_added_df;
 
+def add_poster(df):
+    for i,row in tqdm(df.iterrows(),total=df.shape[0]):
+        tmdb_id=row['tmdbId']
+        tmdb_url=f"https://api.themoviedb.org/3/movie/{tmdb_id}?api_key=0d1228a6e3b2e2cbd8dd8c61420346c5&language=en-US"
+        result=requests.get(tmdb_url)
+        try:
+            df.at[i,"poster_path"] = "https://image.tmdb.org/t/p/original"+result.json()['poster_path']
+            time.sleep(0.1)
+        except(TypeError,KeyError) as e:
+            df.at[i,"poster_path"] = "https://image.tmdb.org/t/p/original/uXDfJbP4ijW5jWSBrPrlKpxab.jpg"
+    return df
+
 if(__name__=="__main__"):
     movies_df=pd.read_csv('data/movies.csv');
     # id를 문자로 인식하게 만든다
@@ -24,10 +39,7 @@ if(__name__=="__main__"):
     merged_df=movies_df.merge(links_df,on='movieId',how='left');
     merged_df['url']=merged_df['imdbId'].apply(lambda x:add_url(x))
     result_df=add_rating(merged_df)
-    
-    print(merged_df);
-    print(merged_df.columns)
-    print("-------------------------------\n\n")
-    print(merged_df.iloc[1,:])
-    print(result_df);
+    result_df["poster_path"]=None
+    result_df=add_poster(result_df)
+    result_df.to_csv("data/movies_final.cvs",index=None)
     
